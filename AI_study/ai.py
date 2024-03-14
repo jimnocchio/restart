0fb30bcb2bb9fd4044b693c6cc27109b369657a2
Gam <rlawlals37@naver.com>
2024-03-14 23:46:27 +0900

    second

--- AI_study/ai.py
+++ AI_study/ai.py
@@ -1,9 +1,19 @@
-#출력값: 2^1000의 각 자릿수를 모두 더하면?
-#풀이: 우선 포문으로 계싼을하고 그걸 리스트로 만들어서 각 리스트끼리 덧셈을한다.
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-result=0
-num=str(2**1000)
-for i in num[0:]:
-    result+=int(i)
-print(result)
-    
+text=['나는 기분이 좋아','나는 짜증나','존나 화나네','기모찌']
+labels=[1,0,0,1]
+
+text_train, text_test, y_train, y_test=train_test_split(text, labels, test_size=0.2, random_state=43)
+
+ct=CountVectorizer()
+X_train=ct.fit_transform(text_train)
+X_test=ct.transform(text_test)
+
+model=MultinomialNB()
+model.fit(X_train,y_train)
+
+predictions=model.predict(X_test)
+print("정확도",accuracy_score(y_test,predictions))
